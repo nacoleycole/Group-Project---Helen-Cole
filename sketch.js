2265cd6af0291251a4f89ab850c3664b24f57b8f
nacoleycole <72955334+nacoleycole@users.noreply.github.com>
2020-10-29 19:36:51 -0700

    first

---
+++ sketch.js
@@ -0,0 +1,52 @@
+// Code in this function is run once, when the sketch is started.
+z = 600
+function setup() {
+  createCanvas(z, z);
+}
+
+class Rectanglez {
+  constructor(x,y,XSideLength,YSideLength){
+    this.x=x;
+    this.y=y;
+    this.XSideLength=XSideLength;
+    this.YSideLength=YSideLength;
+   
+    
+  }
+  colorIt(){
+    fill(random(255),random(255),random(255),50)
+  }
+  drawRectangles(){
+   
+    this.sqrFill(this.x,this.y,this.XSideLength,this.YSideLength);
+  }
+  sqrFill(x,y,xLength,yLength){
+    if(xLength<5||yLength<5){return;}
+     this.colorIt();
+    rect(x,y,xLength,yLength);
+    let dec = random (5,14);
+    let add = dec/2;
+
+    this.sqrFill(x+add,y+add,xLength-dec,yLength-dec)
+  }
+}
+function getSquares(){
+  let Xl = width/4;
+  let Yl= width/4;
+  let squares=[];
+  for (let i=0;i<4;i++){
+    for(let j=0;j<4;j++){
+      squares.push(new Rectanglez(i*Xl,j*Yl,Xl,Yl));
+    }
+  }
+  return squares;
+}
+// Code in this function is run once per frame. If it draws the same thing each
+// time, the sketch is a static image. If it draws something different on
+// different frames, the sketch is an animation.
+function draw() {
+  background(220);
+  let squares= getSquares();
+  squares.forEach(x => x.drawRectangles())
+  noLoop();
+}
