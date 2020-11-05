744fdaa39a553e616e70c253947cd27fa94b2d2d
nacoleycole <72955334+nacoleycole@users.noreply.github.com>
2020-11-04 19:01:28 -0800

    individual square vidual effects

--- sketch.js
+++ sketch.js
@@ -12,6 +12,9 @@ var squares;
  }
 function setup() {
   let canvas = createCanvas(z, z);
+  // background(200);
+  squares= getSquares();
+  squares.forEach(x => x.drawRectangles())
   
 }
 // this class setsup the blueprint for the pattern
@@ -21,58 +24,72 @@ class Rectanglez {
     this.y=y;
     this.XSideLength=XSideLength;
     this.YSideLength=YSideLength;
-    this.sqrDiff= 0 
+    this.sqrDiff= 50; 
+    this.variable = false
+    this.rgb = [0,0,0]
+
+
   }
   //takes care of the colors
   colorIt(){
-    fill(random(255),random(255),random(255),50)
+    fill(random(this.rgb[0], 255),random(this.rgb[1], 255),random(this.rgb[2], 255))
   }
   // actually draws out the pattern
   drawRectangles(){
     this.sqrFill(this.x,this.y,this.XSideLength,this.YSideLength);
     
+    
   }
   // this fill each larger square as they are being visited one by one 
   sqrFill(x,y,xLength,yLength){
-    if(xLength<20||yLength<20){return;}
+    if(xLength<17||yLength<17){return;}
+    this.rgbChange(); 
      this.colorIt();
     rect(x,y,xLength,yLength);
     noStroke();
-    // ellipse(x/2,y/2,xLength/2,yLength);
-    let dec = random (5,30);
+    let dec = random (5,15);
     let add = dec/2;
 
     this.sqrFill(x+add,y+add,xLength-dec,yLength-dec)
-    this.sqrDiff+=add;
+    
   }
 
   getClicked(x, y){
-    console.log(x, y, this.x+this.sqrDiff, this.x+this.XSideLength-this.sqrDiff, this.y +this.YSideLength - this.sqrDiff, this.y+this.sqrDiff)
-      if((x >= (this.x+this.XSideLength-this.sqrDiff) && x <= (this.x+this.sqrDiff)) && (y <= (
-        this.y+this.sqrDiff)) && (y >= (this.y +this.YSideLength - this.sqrDiff))){
+    // console.log(this.x, this.y, this.x+this.sqrDiff, this.x+this.XSideLength-this.sqrDiff, this.y +this.YSideLength - this.sqrDiff, this.y+this.sqrDiff)
+      //without this if statment the user could click anywhere on the canvas and it would make sound- with it sound only comes when clicked in the cente squares
+    if((x >= (this.x+this.sqrDiff) && x <= (this.x+this.XSideLength-this.sqrDiff)) && (y <= (this.y +this.YSideLength - this.sqrDiff)) && (y >= (this.y+this.sqrDiff))){
       console.log('playing');
       sound1.play();
+       // this is the line that makes the patter redraw after the user clicks
+      this.clicked = !this.clicked;
+      this.rgbChange();
+        }
+    }
 
-       /// I THINK this is where it would make sense to add your effects that you want to happen once its clicked 
-      //if you want the effects to only happen when clicked in the middle, then you can leave the if statment as i and
-      //just add your code right in this space 
-      // if you want it to have this effect when clikced in other places then you can add another while or if statment
-      // I also think that if you add your mouse x mouse y function as a method in this class, and call it at the appropriate 
-      //time that should work as well
-      this.sqrDiff = 0;
-      this.drawRectangles(); // this is the line that makes the patter redraw after the user clicks
-        }  
-        this.x+this.sqrDiff
-      }
-   }
+  rgbChange(){
+    this.rgb[0] = (this.rgb[0] + 53) % 200;
+    this.rgb[1] = (this.rgb[1] + 77) % 200;
+    this.rgb[2] = (this.rgb[2] + 36) % 200;
+  }
+  VisualEffect(){
+    if(this.clicked){
+      this.drawRectangles();
+    }
+  }
+  
+}
+
+          
+      
+  
 
 // the double for loops in this fucntion makes it so all of the larger squares are visited one by one 
 function getSquares(){
   let Xl = width/4;
-  let Yl= width/4;
+  let Yl= height/4;
   squares=[];
-  for (let i=0;i<160;i++){
-    for(let j=0;j<10;j++){
+  for (let i=0;i<4;i++){
+    for(let j=0;j<4;j++){
       squares.push(new Rectanglez(i*Xl,j*Yl,Xl,Yl));
     }
   }
@@ -80,24 +97,22 @@ function getSquares(){
 }
 
 function draw() {
-  background(200);
-  squares= getSquares();
-  squares.forEach(x => x.drawRectangles())
-  // noLoop();
+  squares.forEach(x => x.VisualEffect());
+
 }
 
 
 function mousePressed(){
-  console.log(mouseX, mouseY)
-  squares.forEach(x => x.drawRectangles())
+  // console.log(mouseX, mouseY)
   squares.forEach(x => x.getClicked(mouseX, mouseY))
+  // squares.forEach(x => x.drawRectangles())
 
-  if(isLooping())
-  {
-  noLoop();
-  }
-  else
- {
-  loop();   
-   }
+//   if(isLooping())
+//   {
+//   noLoop();
+//   }
+//   else
+//  {
+//   loop();   
+//    }
 }
